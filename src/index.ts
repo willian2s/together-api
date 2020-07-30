6751aad7ae0f52ad44d8716283511dae2b9493e0
Willian Silva <silva.williansantos@gmail.com>
2020-07-30 11:23:29 -0300

    Switch port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 import routes from './routes'
 
 const app = express()
-var port = process.env.PORT || 3333
+var port = process.env.PORT || 8080
 
 app.use(cors())
 app.use(json())
